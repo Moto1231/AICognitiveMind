e39c77b3805ab84c3aff350cce3dcd7ddccdfeb8
WilliamEnright <69278562+Moto1231@users.noreply.github.com>
2026-09-19 06:22:19 -0500

    Add embodiment package

---
+++ src/aicognitive_mind/embodiment/__init__.py
@@ -0,0 +1,19 @@
+"""Embodiment contracts for the Digital Being's sensory and expressive faculties."""
+
+from aicognitive_mind.embodiment.domain import (
+    DeviceStatus,
+    ExpressionIntent,
+    ExpressionModality,
+    Percept,
+    SensoryModality,
+)
+from aicognitive_mind.embodiment.runtime import EmbodimentRuntime
+
+__all__ = [
+    "DeviceStatus",
+    "EmbodimentRuntime",
+    "ExpressionIntent",
+    "ExpressionModality",
+    "Percept",
+    "SensoryModality",
+]
